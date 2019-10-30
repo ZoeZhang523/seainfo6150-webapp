899951d018098ab1c533c1c745b92144f7471c19
“ZoeZhang523” <839921303@qq.com>
2019-10-30 04:09:57 -0700

    Assignment8

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SlugButton from "./SlugButton";
 import './ArticleListItem.module.css'
 import ArticleImage from './ArticleImage';
+import style from "./ArticleListItem.module.css";
 
 const ArticleListItem = props => {
     return (         
@@ -10,14 +11,14 @@ const ArticleListItem = props => {
                 <title>Articles</title>
             </head>
             <body>
-                <section>
+                <section className = {style.container}>
                     <div>
                         <ArticleImage url = {props.article.image._url} title={props.article.image.title}></ArticleImage>
-                    </div>
-                    <div>
                         <h1>{props.article.title}</h1>
                         <p>{props.article.shortText}</p>
-                        <time dateTime = {props.article.pubYear}>{props.article.pubDate}</time>             
+                        <time dateTime = {props.article.pubYear}>{props.article.pubDate}</time>                            
+                    </div>
+                    <div className={style.button}>
                         {<SlugButton article = {props.article}/>}
                     </div>
                 </section>
--- src/ArticleList/SlugButton.jsx
+++ src/ArticleList/SlugButton.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './SlugButton.module.css'
+import style from "./SlugButton.module.css";
 
 const SlugButton = (props) => {
     return (
-        <button onClick = {() => alert(props.article.slug)}>
+        <button className={style.button} onClick = {() => alert(props.article.slug)}>
             {props.article.author}
         </button>             
     );
